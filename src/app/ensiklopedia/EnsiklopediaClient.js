e2e5e1b09c03ff538281d733442f293cdb5b74e8
Pindraa <kadekpindra@gmail.com>
2025-04-13 00:34:45 +0800

    Revise View

--- src/app/ensiklopedia/EnsiklopediaClient.js
+++ src/app/ensiklopedia/EnsiklopediaClient.js
@@ -26,7 +26,7 @@ export default function EnsiklopediaClient({ cultures }) {
       className="max-w-4xl mx-auto py-10 px-4 sm:px-6 lg:px-8 overflow-hidden"
     >
       <h2 className="text-3xl font-bold text-center text-blue-900 fade-in">
-        Event Budaya
+        Ensiklopedia
       </h2>
       <br />
       <div className="space-y-6 flex flex-col">
