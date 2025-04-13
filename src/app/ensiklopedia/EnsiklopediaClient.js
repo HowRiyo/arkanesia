9a2917af8b9ffea6885d9c1b7152550b99042f81
HowRiyo <howriyo@gmail.com>
2025-04-13 16:49:29 +0800

    update

--- src/app/ensiklopedia/EnsiklopediaClient.js
+++ src/app/ensiklopedia/EnsiklopediaClient.js
@@ -2,7 +2,6 @@
 import { useEffect, useRef, useState } from "react";
 import gsap from "gsap";
 import Link from "next/link";
-import Image from "next/image";
 
 export default function EnsiklopediaClient({ cultures }) {
   const wrapperRef = useRef(null);
@@ -30,33 +29,51 @@ export default function EnsiklopediaClient({ cultures }) {
   return (
     <main
       ref={wrapperRef}
-      className="max-w-4xl mx-auto py-10 px-4 sm:px-6 lg:px-8 overflow-hidden"
+      className="py-30 px-30 overflow-hidden"
     >
-      <h2 className="text-3xl font-bold text-center text-blue-900 fade-in">
-        Event Budaya
+      <h2 className="text-3xl font-bold text-center text-[var(--main-col)] fade-in">
+        Ensiklopedia
       </h2>
-      <br />
-      <div className="space-y-6 flex flex-col">
-        {cultures.map((culture, i) => (
+
+      <div className="my-15 fade-in">
+        <input
+          type="text"
+          placeholder="Cari Budaya di Provinsi..."
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+          className="w-full border-[var(--border-col)] border rounded-md px-4 py-2 focus:outline-none focus:ring-2 focus:ring-[var(--accent-col)] transition"
+        />
+      </div>
+
+      <div className="space-y-6 flex flex-col gap-5">
+        {filteredCultures.map((culture, i) => (
           <Link
             href={`/ensiklopedia/${culture.id}`}
             key={i}
             className="fade-in"
           >
-            <div className="bg-white p-4 shadow-md rounded-lg flex flex-col sm:flex-row items-start gap-4">
+            <div className="p-4 shadow-md rounded-lg flex flex-col sm:flex-row items-start gap-4">
               <div
-                className="w-16 h-16 min-w-[64px] bg-gray-200 rounded-md bg-cover bg-center"
+                className="w-52 h-32 min-w-[64px] bg-[var(--light-col)] rounded-md bg-cover bg-center"
                 style={{ backgroundImage: `url(${culture.image})` }}
               ></div>
-              <div>
-                <h3 className="font-bold text-gray-800">
+              <div className="flex-1">
+                <h3 className="font-bold text-[var(--dark-col)]">
                   {culture.title} ({culture.year})
                 </h3>
-                <p className="text-gray-600 text-sm">{culture.description}</p>
+                <p className="text-[var(--gray-col)] text-sm line-clamp-4">
+                  {culture.description}
+                </p>
               </div>
             </div>
           </Link>
         ))}
+
+        {filteredCultures.length === 0 && (
+          <p className="text-center text-[var(--gray-col)]">
+            Tidak ditemukan hasil untuk lokasi tersebut.
+          </p>
+        )}
       </div>
     </main>
   );
