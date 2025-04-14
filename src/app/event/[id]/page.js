2259b34df2d81af14ed6f70695fed46094dbebc5
Pindraa <kadekpindra@gmail.com>
2025-04-14 21:15:35 +0800

    Revise View

--- src/app/event/[id]/page.js
+++ src/app/event/[id]/page.js
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export default async function EventDetailPage({ params }) {
   const { id } = params;
 
@@ -12,17 +14,17 @@ export default async function EventDetailPage({ params }) {
   }
 
   return (
-    <main>
+    <main className="mx-auto max-w-6xl py-10 px-4 sm:px-6 lg:px-8">
       <div className="flex items-center justify-between mb-4">
         <Link
-          href="/ensiklopedia"
+          href="/event"
           className="bg-[var(--accent-col)] text-[var(--light-col)] px-6 py-2 rounded-lg shadow-[0_2px_4px_var(--shadow-col)] transition"
         >
           Back
         </Link>
       </div>
       <h2 className="text-3xl font-bold mb-4">
-        {event.title} ({event.year})
+        {event.name}
       </h2>
       <img
         src={event.image}
@@ -30,7 +32,7 @@ export default async function EventDetailPage({ params }) {
         className="w-full h-auto rounded-lg my-6"
       />
       <div className="flex flex-col items-center">
-        <p className="bg-[var(--dark-col)] p-4 text-[var(--gray-col)] leading-relaxed text-center">
+        <p className="p-4 text-[var(--gray-col)] leading-relaxed text-center">
           Gambar ini menunjukkan ilustrasi terkait ensiklopedia yang sedang
           dibahas. Visualisasi ini membantu memahami konteks lebih dalam dan
           memperkaya informasi yang tersedia.
