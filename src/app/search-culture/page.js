b794f3e11a8493ff2a78fe7222466e8d1810dfa6
Pindraa <kadekpindra@gmail.com>
2025-04-16 02:54:29 +0800

    Update Link

--- src/app/search-culture/page.js
+++ src/app/search-culture/page.js
@@ -26,7 +26,7 @@ export default function SearchPage() {
       input.value = "";
 
       try {
-        const res = await fetch("http://localhost:5001/chatbot", {
+        const res = await fetch("https://arkanesia.vercel.app/chatbot", {
           method: "POST",
           headers: { "Content-Type": "application/json" },
           body: JSON.stringify({ question: userMessage }),
